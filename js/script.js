2a67dfc00205a633281937f28564fb73d645df27
ruben-ng <rubenkece2@gmail.com>
2024-08-01 20:33:44 +0700

    mulai responsve

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+// Initialization for ES Users
+import {
+  Carousel,
+  initTWE,
+} from "tw-elements";
 
-
+initTWE({ Carousel });
+    
     document.addEventListener('DOMContentLoaded', function () {
         const trigger = document.getElementById('popup_trigger');
         const container = document.getElementById('popup_container');
@@ -29,7 +14,6 @@ const swiper = new Swiper('.swiper', {
             // Toggle visibility by adding/removing the 'hidden' class
             container.classList.toggle('hidden');
         });
-
         // Optional: Click outside the popup to close it
         document.addEventListener('click', function (event) {
             if (!trigger.contains(event.target) && !container.contains(event.target)) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html"],
+  content: [
+    "./**/*.html",
+    "./node_modules/tw-elements/js/**/*.js"
+  ],
   theme: {
     extend: {
       screens:{
@@ -37,5 +40,8 @@ module.exports = {
     },
   },
 
-  plugins: [],
+  plugins: [
+    require("tw-elements/plugin.cjs")
+  ],
+    darkMode: "class"
 }
